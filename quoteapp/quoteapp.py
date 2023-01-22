2ee2a517dd197152bdf40f1dab993c6474de22e5
Raghavendra Deshmukh <raghavendra.deshmukh@gmail.com>
2023-01-22 13:33:09 +0530

    Update quoteapp.py
    Jan'23

--- quoteapp/quoteapp.py
+++ quoteapp/quoteapp.py
@@ -1,19 +1,33 @@
-#include the required Libraries
 from google.cloud import bigquery
 import pandas as pd
 from flask import Flask, render_template, request, jsonify, make_response
 from datetime import datetime
 import random
 import hashlib
+import json
+from flask_cors import CORS
+from google.cloud import pubsub_v1
+
+# Create a Flask App
+app = Flask(__name__)
+CORS(app)
+
+# Set up the Cloud Pub/Sub client
+publisher = pubsub_v1.PublisherClient()
+# Get the topic handle
+project = 'deshmukhr-cep'
+topic_name = 'rdtopic1'
+# Setup the Topic Path to Publish the Information
+topic_path = publisher.topic_path(project, topic_name)
+
+# Initialize the JSON Encoder object
+js = json.JSONEncoder()
 
 #Setup the BQ Client
 client = bigquery.Client()
 
 #Define Table details
 table_id = "deshmukhr-cep.TESTRD.Quotes"
-
-# Create a Flask App
-app = Flask(__name__)
 
 #Routine to get Current Time
 def getCurrTime():
@@ -36,8 +50,20 @@ def addDFToDB(quotesDF:pd.DataFrame):
 
 #Routine to add one Quote to the Database
 def addQuoteToDB(row_to_insert):
-    errors = client.insert_rows_json(table_id, row_to_insert)  # Make an API request.
+    #job_config = bigquery.LoadJobConfig(write_disposition='WRITE_APPEND',create_disposition='CREATE_NEVER',streaming=True,publish_callback=topic.publish)
+    
+    errors = client.insert_rows_json(table_id, row_to_insert) #, job_config=job_config)  # Make an API request.
     return errors
+
+#function to send Quote Data to a Pub/Sub Topic --> Not in use
+def publishToTopic(quoteData):
+    # We need to Encode it (default utf-8) to be sent to the Pub/Sub Topic
+    pubsub_data = js.encode(quoteData)
+    pubsub_data = pubsub_data.encode()
+    print(pubsub_data)
+    # Send it to the Pub/Sub Topic
+    future = publisher.publish(topic_path, pubsub_data)
+
 
 #Routine to Get the Quotes from the DB and send it in a Dataframe
 def getQuotesDF():
@@ -48,15 +74,26 @@ def getQuotesDF():
 #Global Object (oops!!!) to store the Quotes DataFrame
 quotesDF = getQuotesDF()
 
+#On successful addition of a new Quote to the DB, the same will be updated to the Cache - quotesDF
+def updateQuotesDF(quoteData):
+    global quotesDF
+    tempDF = pd.DataFrame(quoteData)
+    print(tempDF)
+    quotesDF = pd.concat([quotesDF, tempDF])
+    quotesDF = quotesDF.reset_index(drop=True)
+
+
+#Default: Renders Index.html
 @app.route('/')
 def home():
-    quotesDF.count()
     return render_template('index.html')
 
+#Gets all quotes and presents them in a Table
 @app.route('/getAllQuotes')
 def getAllQuotes():
     return render_template('table.html', tables=[quotesDF.to_html()], titles=[''])
 
+#Gets a Random Quote
 @app.route('/getRandomQuote')
 def getRandomQuote():
     randInt = random.randint(0, 3000)
@@ -64,18 +101,25 @@ def getRandomQuote():
     row = quotesDF.loc[randInt]
     randomQuote = row["Quote"]
     quoteAuthor = row["Author"]
-    quoteText = f"{randomQuote} - {quoteAuthor}"
-    return jsonify (quoteText)
+    #quoteText = f"{randomQuote} - {quoteAuthor}"
+    quoteText = {"Quote":randomQuote, "Author":quoteAuthor}
+    
+    response = jsonify (quoteText)
+    # Enable Access-Control-Allow-Origin
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
 
+#Adds a New Quote to the Database
 @app.route('/addQuote', methods=['POST'])
 def addQuote():
     quoteJSON = request.get_json()
+    print(quoteJSON)
     if 'Quote' not in quoteJSON:
         return make_response('Quote is required', 400)
     if 'Author' not in quoteJSON:
         return make_response('Author is required', 400)   
-    quote = quoteJSON['Quote']
-    author = quoteJSON['Author']
+    quote = str(quoteJSON['Quote'])
+    author = str(quoteJSON['Author'])
     source = quoteJSON['Source']
     tags = quoteJSON['Tags']
     likes = quoteJSON['Likes']
@@ -85,11 +129,80 @@ def addQuote():
 
     errors = addQuoteToDB(quoteData)
 
+    # Set the CORS headers
+    headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': '*'
+    }
+    
     if errors == []:
-        return make_response('Quote Data Saved in DB', 200)
+        #pub_response = publishToTopic(quoteData)
+        updateQuotesDF(quoteData)
+        response = make_response('Quote Data Saved in DB', 200)
     else:
         errorMessage = f"Error while Saving Quote Data - {errors}"
-        return make_response(errorMessage, 400)
+        response = make_response(errorMessage, 400)
+    return response
+
+#Get the Basic details of the # of Quotes, # of Authors
+@app.route('/getBasicDetails', methods=['GET'])
+def getBasicDetails():
+    uniqueAuthors = len(pd.unique(quotesDF['Author'])) #Get unique Authors. https://www.geeksforgeeks.org/how-to-count-distinct-values-of-a-pandas-dataframe-column/
+    totalQuotes = len(quotesDF.axes[0]) #Get the count of Rows
+    quotesBasicInfo = {"TotalQuotes":totalQuotes, "UniqueAuthors":uniqueAuthors}
+    print(quotesBasicInfo)
+    response = jsonify(quotesBasicInfo)
+    # Enable Access-Control-Allow-Origin
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+@app.route('/getAuthorList', methods=['GET'])
+def getAuthorList():
+    uniqueAuthors = pd.unique(quotesDF['Author'])
+    uniqueAuthors.sort()
+    
+    authorList = json.dumps(uniqueAuthors.tolist())
+    response = make_response(authorList, 200)
+    # Enable Access-Control-Allow-Origin
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+
+#To find a Random Quote From a chosen Author
+@app.route('/getRandomQuoteForAuthor/<author>', methods=['GET'])
+def getRandomQuoteForAuthor(author):
+    indexArray = []
+    for index, row in quotesDF.iterrows():
+        if row['Author'] == author:
+            indexArray.append(index)
+    
+    randInt = random.randint(0,len(indexArray)-1)      
+    print(randInt)
+    rowID = indexArray[randInt]
+    print(rowID)
+    quoteText = quotesDF.loc[rowID]['Quote']
+    quoteResponse = {'Quote': quoteText}
+    response = make_response(quoteResponse, 200)
+    # Enable Access-Control-Allow-Origin
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+    
+    '''
+    quoteRow = quotesDF.loc[quotesDF['Author'] == author]
+    print(type(quoteRow))
+    jsontext = quoteRow.to_json()
+    print(jsontext)
+    jsontext = json.loads(jsontext)
+    print(type(jsontext))
+    '''
+    '''
+    quoteText = quoteRow['Quote']
+    print(type(quoteText))
+    str = quoteText.to_string()
+    print(str)
+    print(type(str))
+    '''
+
 
 
 if __name__ == '__main__':
